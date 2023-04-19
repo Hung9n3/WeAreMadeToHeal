8f48b2bc6fc89d191059d2fb203867e07f8ede92
Aspodel D <dattran2309@gmail.com>
2023-04-19 12:22:43 +0700

    add excel handler

---
+++ WeAreMadeToHeal/Helpers/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using WeAreMadeToHeal;
+using WeAreMadeToHeal.Helpers.Email;
+
+namespace WeAreMadeToHeal;
+
+public class ServiceExtensions
+{
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<EmailConfig>();
+        services.AddTransient<EmailHelper>();
+        services.AddTransient<ExcelHandlerService>();
+    }
+}
